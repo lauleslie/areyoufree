e3e9cee6ed006fc6ff52284d07532c9e5812e2b1
Jenna Wu <jennawu0912@users.noreply.github.com>
2016-11-21 03:22:55 -0800

    Add files via upload

--- areyoufree/models/tables.py
+++ areyoufree/models/tables.py
@@ -9,20 +9,18 @@
 
 import datetime
 
-def get_user_email():
-    return auth.user.email if auth.user else None
-
-db.define_table('track',
-                Field('artist'),
-                Field('album'),
-                Field('title'),
-                Field('duration', 'float'),
-                Field('rating', 'float'),
-                Field('num_plays', 'integer'),
-                Field('created_by', default=get_user_email()),
-                Field('created_on', default=datetime.datetime.utcnow()),
+db.define_table('post',
+                Field('user_email', default=auth.user.email if auth.user_id else None),
+                Field('post_content', 'text'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
+                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
                 )
 
+# I don't want to display the user email by default in all forms.
+db.post.user_email.readable = db.post.user_email.writable = False
+db.post.post_content.requires = IS_NOT_EMPTY()
+db.post.created_on.readable = db.post.created_on.writable = False
+db.post.updated_on.readable = db.post.updated_on.writable = False
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
